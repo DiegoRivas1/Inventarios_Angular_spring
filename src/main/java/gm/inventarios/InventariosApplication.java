70cce68cc4cf4fd3830d354ca10217852c302992
DiegoRivas1 <rhyi888@gmail.com>
2026-03-24 02:42:43 -0500

    Creacion de la capa repsotiorio y servicio con sus respectivas clases e interfaces

--- src/main/java/gm/inventarios/InventariosApplication.java
+++ src/main/java/gm/inventarios/InventariosApplication.java
@@ -1,5 +1,6 @@
 package gm.inventarios;
 
+import gm.inventarios.modelo.Producto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class InventariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventariosApplication.class, args);
+
+		//Prueba lombok
+		Producto producto = new Producto();
+		producto.setDescripcion("Camisa");
+		producto.setPrecio(600.0);
+		producto.setExistencia(120);
+
+		//Imprimir
+		System.out.println("producto = " + producto);
 	}
 
 }
