9b08fc2dd48980f88bd48d1dc456e006eed8ed00
DiegoRivas1 <rhyi888@gmail.com>
2026-03-27 18:43:01 -0500

    feat: Creacion de producto-lista y producto form forntend

--- inventario-app/src/app/modelos/producto.ts
+++ inventario-app/src/app/modelos/producto.ts
@@ -1,17 +1,19 @@
-export interface Producto {
-    idProducto?: number;// Es opcional porque se genera automáticamente en la base de datos
-    descripcion: string;
-    precio: number;
-    existencia: number;
-
-}
-
-/*
 export class Producto {
     idProducto?: number;// Es opcional porque se genera automáticamente en la base de datos
     descripcion!: string; // ! indica que esta propiedad se inicializará posteriormente, evitando el error de "Property has no initializer and is not definitely assigned in the constructor."
     precio!: number;
     existencia!: number;
+
+}
+
+/*
+
+
+export interface Producto {
+    idProducto?: number;// Es opcional porque se genera automáticamente en la base de datos
+    descripcion: string;
+    precio: number;
+    existencia: number;
 
 }
 
