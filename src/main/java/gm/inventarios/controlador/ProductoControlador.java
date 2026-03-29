0ddd742d4afc946d798f33b7c4eebc1bf5336dd0
DiegoRivas1 <rhyi888@gmail.com>
2026-03-29 01:59:36 -0500

    Creacion del CRUD RESTANTE y enviando datos al frontend.

--- src/main/java/gm/inventarios/controlador/ProductoControlador.java
+++ src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -5,10 +5,8 @@ import gm.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,7 @@ public class ProductoControlador {// Mnadar informacion al login de nuestra apli
     @Autowired
     private ProductoServicio productoServicio;
 
+    //http://localhost:8080/inventario-app/productos
     @GetMapping("/productos") // http://localhost:8080/inventario-app/productos
     public List<Producto> obtenerProductos() {
         List<Producto> productos = this.productoServicio.listarProductos();
@@ -28,6 +27,60 @@ public class ProductoControlador {// Mnadar informacion al login de nuestra apli
         productos.forEach(producto -> logger.info(producto.toString()) );
         return productos;
     }
+
+    //GET por id http://localhost:8080/inventario-app/productos/1
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable Integer id){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if( producto != null){
+            logger.info("Producto encontrado: {}", producto);
+            return ResponseEntity.ok(producto);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    // POST crear  http://localhost:8080/inventario-app/productos
+    @PostMapping("/productos")
+    public ResponseEntity<Producto> insertarProducto(@RequestBody Producto producto){
+        Producto productoGuardado = this.productoServicio.guardarProducto(producto);
+        logger.info("Producto guardado: {}", productoGuardado);
+        return ResponseEntity.ok(productoGuardado);
+    }
+
+    // PUT actualizar  http://localhost:8080/inventario-app/productos/1
+    /*
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable Integer id, @RequestBody Producto producto){
+        Producto productoBuscado = this.productoServicio.buscarProductoPorId(id);
+        if (productoBuscado == null) return ResponseEntity.notFound().build();
+
+        productoBuscado.setDescripcion(producto.getDescripcion());
+        productoBuscado.setPrecio(producto.getPrecio());
+        productoBuscado.setExistencia(producto.getExistencia());
+
+        this.productoServicio.guardarProdcuto(productoBuscado);
+        logger.info("Producto actualizado: {}", productoBuscado);
+        return ResponseEntity.ok(productoBuscado);
+    }
+    */
+    // PUT actualizar  http://localhost:8080/inventario-app/productos/1
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable Integer id, @RequestBody Producto producto) {
+        producto.setIdProducto(id);
+        Producto productoActualizado = this.productoServicio.guardarProducto(producto);
+        logger.info("Producto actualizado: {}", productoActualizado);
+        return ResponseEntity.ok(productoActualizado);
+    }
+    // DELETE eliminar  http://localhost:8080/inventario-app/productos/1
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Void> eliminarProducto(@PathVariable Integer id){
+        this.productoServicio.eliminarProductoPorId(id);
+        logger.info("Producto eliminado: {}", id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
 
 
 
